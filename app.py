4e123edbb546e32c4b29e4201428c37105ef6c8d
Aarav Bansal <73339570+bansalaarav@users.noreply.github.com>
2023-06-22 12:45:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
+
 st.set_page_config(
     page_title="Video games analysis",
     layout="wide",
@@ -9,7 +10,9 @@ st.set_page_config(
 
 st.title("Video Games Analysis Dashboard")
 
+
 DATA_PATH = "game_sales.csv"
+
 
 @st.cache_data
 def load_data(nrows):
@@ -18,7 +21,6 @@ def load_data(nrows):
 
 
 selected = False
-
 
 with st.sidebar:
     st.title("Options")
@@ -36,7 +38,6 @@ with st.sidebar:
     st.markdown("---")
 
     st.header("Plots")
-
 
     st.subheader("Based on number of games")
     add_publishers_ngames = st.checkbox("Publishers", key=0)
@@ -62,38 +63,30 @@ if overall_insights:
 - 2009 was a popular year for video games, with the most number of games released.
 - While in other regions, action is the most popular genre, in Japan it is role-playing.
 - The number of games released per year has decreased exponentially since 2009, with a small increase in 2015. This could be due to the rise of mobile gaming.
-    
     """)
 
     st.write("")
     st.write("")
 
-    # st.write("2008 was a popular year for video games, with the most number of games released.")
-    # st.write("While in other regions, action is the most popular genre, in Japan it is role-playing.")
-    # st.write("The number of games released per year has decreased exponentially since 2008. This could be due to the rise of mobile gaming.")
-    
 
-
-# Load nrows of data (from number input)
+# Load nrows of data
 data = load_data(nrows)
+
 # Only use data from the entered year range (from slider)
 data = data[(data['Year'] >= year_range[0]) & (data['Year'] <= year_range[1])]
 
+
 charts = {}
-
-
 
 if add_publisher_sales:
     heading5 = st.selectbox("Choose a publisher (for pie chart)", data['Publisher'].unique(), key=99)
 
-    titleee = "Region-wise sales for " + heading5 + " (in millions)"
+    title = "Region-wise sales for " + heading5 + " (in millions)"
 
     NA_sales = data[data['Publisher'] == heading5]['NA_Sales'].sum()
     EU_sales = data[data['Publisher'] == heading5]['EU_Sales'].sum()
     JP_sales = data[data['Publisher'] == heading5]['JP_Sales'].sum()
     Other_sales = data[data['Publisher'] == heading5]['Other_Sales'].sum()
-
-    # Create altair pie chart
 
     df = pd.DataFrame({'Region': ['North America', 'Europe', 'Japan', 'Other'], 'Sales': [NA_sales, EU_sales, JP_sales, Other_sales]})
 
@@ -104,23 +97,19 @@ if add_publisher_sales:
 
     conclusion_card_0 = f"For the {heading5} publisher, maximum sales are {max(NA_sales, EU_sales, JP_sales, Other_sales)} million from the region {'North America' if max(NA_sales, EU_sales, JP_sales, Other_sales) == NA_sales else 'Europe' if max(NA_sales, EU_sales, JP_sales, Other_sales) == EU_sales else 'Japan' if max(NA_sales, EU_sales, JP_sales, Other_sales) == JP_sales else 'Other'}."
 
-    charts[titleee] = [chart5, conclusion_card_0]
-
-    # print(NA_sales)
+    charts[title] = [chart5, conclusion_card_0]
 
 
 if add_genre_sales:
     heading6 = st.selectbox("Choose a genre (for pie chart)", data['Genre'].unique(), key=98)
 
-    titleee2 = "Region-wise sales for " + heading6 + " (in millions)"
+    title2 = "Region-wise sales for " + heading6 + " (in millions)"
 
 
     NA_sales = data[data['Genre'] == heading6]['NA_Sales'].sum()
     EU_sales = data[data['Genre'] == heading6]['EU_Sales'].sum()
     JP_sales = data[data['Genre'] == heading6]['JP_Sales'].sum()
     Other_sales = data[data['Genre'] == heading6]['Other_Sales'].sum()
-
-    # Create altair pie chart
 
     df = pd.DataFrame({'Region': ['North America', 'Europe', 'Japan', 'Other'], 'Sales': [NA_sales, EU_sales, JP_sales, Other_sales]})
 
@@ -131,14 +120,13 @@ if add_genre_sales:
 
     conclusion_card_1 = f"In the {heading6} genre, maximum sales are {max(NA_sales, EU_sales, JP_sales, Other_sales)} million from the region {'North America' if max(NA_sales, EU_sales, JP_sales, Other_sales) == NA_sales else 'Europe' if max(NA_sales, EU_sales, JP_sales, Other_sales) == EU_sales else 'Japan' if max(NA_sales, EU_sales, JP_sales, Other_sales) == JP_sales else 'Other'}."
 
-
-    charts[titleee2] = [chart6, conclusion_card_1]
+    charts[title2] = [chart6, conclusion_card_1]
 
 
 if add_platform_sales:
     heading7 = st.selectbox("Choose a platform (for pie chart)", data['Platform'].unique(), key=97)
 
-    titleee3 = "Region-wise sales for " + heading7 + " (in millions)"
+    title3 = "Region-wise sales for " + heading7 + " (in millions)"
 
 
     NA_sales = data[data['Platform'] == heading7]['NA_Sales'].sum()
@@ -157,20 +145,18 @@ if add_platform_sales:
 
     conclusion_card_2 = f"On the {heading7} platform, maximum sales are {max(NA_sales, EU_sales, JP_sales, Other_sales)} million from the region {'North America' if max(NA_sales, EU_sales, JP_sales, Other_sales) == NA_sales else 'Europe' if max(NA_sales, EU_sales, JP_sales, Other_sales) == EU_sales else 'Japan' if max(NA_sales, EU_sales, JP_sales, Other_sales) == JP_sales else 'Other'}."
 
-    charts[titleee3] = [chart7, conclusion_card_2]
+    charts[title3] = [chart7, conclusion_card_2]
 
 
 if add_year_sales:
     heading8 = st.selectbox("Choose a year (for pie chart)", [int(i) for i in data['Year'].unique()], key=96)
 
-    titleee4 = "Region-wise sales in " + str(heading8) + " (in millions)"
+    title4 = "Region-wise sales in " + str(heading8) + " (in millions)"
 
     NA_sales = data[data['Year'] == heading8]['NA_Sales'].sum()
     EU_sales = data[data['Year'] == heading8]['EU_Sales'].sum()
     JP_sales = data[data['Year'] == heading8]['JP_Sales'].sum()
     Other_sales = data[data['Year'] == heading8]['Other_Sales'].sum()
-
-    # Create altair pie chart
 
     df = pd.DataFrame({'Region': ['North America', 'Europe', 'Japan', 'Other'], 'Sales': [NA_sales, EU_sales, JP_sales, Other_sales]})
 
@@ -181,7 +167,7 @@ if add_year_sales:
 
     conclusion_card_3 = f"In the year {heading8}, maximum sales are {max(NA_sales, EU_sales, JP_sales, Other_sales)} million from the region {'North America' if max(NA_sales, EU_sales, JP_sales, Other_sales) == NA_sales else 'Europe' if max(NA_sales, EU_sales, JP_sales, Other_sales) == EU_sales else 'Japan' if max(NA_sales, EU_sales, JP_sales, Other_sales) == JP_sales else 'Other'}."
 
-    charts[titleee4] = [chart8, conclusion_card_3]
+    charts[title4] = [chart8, conclusion_card_3]
 
 
 
@@ -192,10 +178,8 @@ if add_publishers_ngames:
     # Get top 10 publishers
     games_per_publisher = data['Publisher'].value_counts().head(10)
 
-    # Create dataframe for altair
     df = pd.DataFrame({'Publisher': games_per_publisher.index, 'Number of Games': games_per_publisher.values})
 
-    # Create altair chart
     chart1 = alt.Chart(df).mark_bar().encode(
         x=alt.X('Publisher', sort='-y', axis=alt.Axis(labelFontSize=16)), # Sort descending
         y=alt.Y('Number of Games', axis=alt.Axis(labelFontSize=16)),
@@ -212,10 +196,8 @@ if add_genres_ngames:
     # Get top 10 genres
     games_per_genre = data['Genre'].value_counts().head(10)
 
-    # Create dataframe for altair
     df = pd.DataFrame({'Genre': games_per_genre.index, 'Number of Games': games_per_genre.values})
 
-    # Create altair chart
     chart2 = alt.Chart(df).mark_bar().encode(
         x=alt.X('Genre', sort='-y', axis=alt.Axis(labelFontSize=16)), # Sort descending
         y=alt.Y('Number of Games', axis=alt.Axis(labelFontSize=16)),
@@ -233,10 +215,8 @@ if add_platforms_ngames:
     # Get top 10 platforms
     games_per_platform = data['Platform'].value_counts().head(10)
 
-    # Create dataframe for altair
     df = pd.DataFrame({'Platform': games_per_platform.index, 'Number of Games': games_per_platform.values})
 
-    # Create altair chart
     chart3 = alt.Chart(df).mark_bar().encode(
         x=alt.X('Platform', sort='-y', axis=alt.Axis(labelFontSize=16)), # Sort descending
         y=alt.Y('Number of Games', axis=alt.Axis(labelFontSize=16)),
@@ -254,10 +234,8 @@ if add_year_ngames:
     # Get number of games per year
     games_per_year = data['Year'].value_counts()
 
-    # Create dataframe for altair
     df = pd.DataFrame({'Year': games_per_year.index, 'Number of Games': games_per_year.values})
 
-    # Create altair chart
     chart4 = alt.Chart(df).mark_bar().encode(
         x=alt.X('Year', axis=alt.Axis(labelFontSize=16)),
         y=alt.Y('Number of Games', axis=alt.Axis(labelFontSize=16)),
@@ -268,25 +246,6 @@ if add_year_ngames:
 
     charts[heading4] = [chart4, conclusion_card_7]
 
-
-
-
-
-# charts_keys = list(charts.keys())
-# num_charts = len(charts_keys)
-
-# with st.container():
-#     num_cols = min(2, num_charts)  # Maximum of 2 columns
-#     cols = st.columns(num_cols)
-
-#     for i in range(num_charts):
-#         with cols[i % num_cols]:
-
-#             st.write(charts_keys[i])
-#             st.altair_chart(charts[charts_keys[i]], use_container_width=True)
-
-
-# Display all the plots with their conclusion cards next to them. (2 columns)
 
 charts_keys = list(charts.keys())
 num_charts = len(charts_keys)
